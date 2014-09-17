323b706bc999f491ef234c1dd1ab70ba4f353203
Zak Angelle <zaknotzak@gmail.com>
2014-09-17 13:46:12 -0500

    Cleanin up shit

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var d3                  = require('d3');
 var D3ConcentricCircles = require('../index.js');
 
 test('basics', function(t) {
-  t.plan(2);
+  t.plan(5);
 
   var data = [
     { label: 'Label', value: 100 },
@@ -14,6 +14,18 @@ test('basics', function(t) {
   var container = document.createElement('div');
   container.classList.add('container');
   document.body.appendChild(container);
+
+  t.throws(function(t) {
+    new D3ConcentricCircles(null, data);
+  }, 'should throw when `selector` is not provided');
+
+  t.throws(function(t) {
+    new D3ConcentricCircles('.container');
+  }, 'should throw when `data` is not provided');
+
+  t.throws(function(t) {
+    new D3ConcentricCircles('body');
+  }, 'should throw if container element is not empty');
 
   t.ok(new D3ConcentricCircles('.container', data),
     'should instantiate');
@@ -25,7 +37,7 @@ test('basics', function(t) {
 });
 
 test('options', function(t) {
-  t.plan(1);
+  t.plan(2);
 
   var data = [
     { label: 'Label', value: 100 },
@@ -48,5 +60,37 @@ test('options', function(t) {
 
   var cc = d3.concentricCircles('.container', data, options);
 
-  t.deepEquals(options, cc.options);
+  t.deepEquals(options, cc.options,
+    'should merge runtime options with defaults');
+
+  document.body.removeChild(container);
+
+  container = document.createElement('div');
+  container.classList.add('container');
+  document.body.appendChild(container);
+
+  cc = d3.concentricCircles('.container', data, {});
+
+  t.deepEquals(cc.options, cc.DEFAULT_OPTIONS,
+    'should use defaults if no options specified');
+
+  document.body.removeChild(container);
+});
+
+test('dom', function(t) {
+  t.plan(1);
+
+  var data = [
+    { label: 'Label', value: 100 },
+    { label: 'Label 2', value: 132 },
+  ];
+
+  var container = document.createElement('div');
+  container.classList.add('container');
+  document.body.appendChild(container);
+
+  var cc = d3.concentricCircles('.container', data);
+  var svg = document.querySelector('.container svg');
+
+  t.ok(svg, 'should create svg element');
 });
