4bacb7e92e7ab94b2cb8f3da8fdb7e73de30b49c
Zak Angelle <zaknotzak@gmail.com>
2014-09-17 14:47:48 -0500

    Cleanin up stuff and tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var d3                  = require('d3');
 var D3ConcentricCircles = require('../index.js');
 
 test('basics', function(t) {
-  t.plan(5);
+  t.plan(4);
 
   var data = [
     { label: 'Label', value: 100 },
@@ -15,23 +15,20 @@ test('basics', function(t) {
   container.classList.add('container');
   document.body.appendChild(container);
 
+  t.ok(isFunction(d3.concentricCircles),
+    'extends d3 with method');
+
   t.throws(function(t) {
-    new D3ConcentricCircles(null, data);
+    d3.concentricCircles(null, data);
   }, 'should throw when `selector` is not provided');
 
   t.throws(function(t) {
-    new D3ConcentricCircles('.container');
+    d3.concentricCircles('.container');
   }, 'should throw when `data` is not provided');
 
   t.throws(function(t) {
-    new D3ConcentricCircles('body');
+    d3.concentricCircles('body');
   }, 'should throw if container element is not empty');
-
-  t.ok(new D3ConcentricCircles('.container', data),
-    'should instantiate');
-
-  t.ok(isFunction(d3.concentricCircles),
-    'extends d3 with method');
 
   document.body.removeChild(container);
 });
