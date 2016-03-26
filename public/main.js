0cf49f99bf4bf48fc7dc918875d96fb1782a153a
Zak Angelle <zaknotzak@gmail.com>
2016-03-26 01:02:17 -0500

    Updated example

--- public/main.js
+++ public/main.js
@@ -1,11 +1,11 @@
 var d3ConcentricCircles = require('../index.js');
 
 var data = [
-  { display: 'Network', value: 77 },
-  { display: 'Hardware', value: 158 },
+  { display: 'Other', value: 77 },
+  { display: 'Apps', value: 158 },
   { display: 'OS', value: 92 },
-  { display: 'Apps', value: 80 },
-  { display: 'Other', value: 194 }
+  { display: 'Hardware', value: 80 },
+  { display: 'Network', value: 194 }
 ];
 
 d3ConcentricCircles('.container', data, {
